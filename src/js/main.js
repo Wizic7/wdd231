0a8d629675623b4ef22b039d2c3b53502d1fd5e7
Wizic7 <IsaacWizic7@gmail.com>
2024-09-21 23:23:49 -0600

    Added JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,32 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+
+
+function init() {
+    updateDisclaimer(parkData);
+    document.querySelector("title").innerHTML=parkData.fullName;
+    document.querySelector(".hero-banner").innerHTML=bannerTemplate(parkData);
+  }
+  init();
+
+function updateDisclaimer(park){
+    let disclaimer = document.querySelector(".disclaimer a");
+    disclaimer.innerHTML=park.fullName;
+    disclaimer.href=park.url;
+}
+
+function bannerTemplate(park){
+    return `
+    <img src="${park.images[0].url}" alt="${park.images[0].altText}" />
+    <div class="hero-banner__content">
+    <a href="#" class="hero-banner__title">${park.name}</a>
+    <p class="hero-banner__subtitle">
+        <span>${park.designation}</span>
+        <span>${park.states}</span>
+    </p>
+    </div>
+    `;
+}
+
